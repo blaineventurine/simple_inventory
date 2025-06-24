35dbc01a5928989ceba6bb4063a06f56f075428f
Blaine Venturine <blaineventurine@gmail.com>
2025-06-23 22:32:11 -0500

    fix CI

--- custom_components/simple_inventory/__init__.py
+++ custom_components/simple_inventory/__init__.py
@@ -1,5 +1,6 @@
 import logging
 
+import homeassistant.helpers.config_validation as cv
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
@@ -24,6 +25,7 @@ from .todo_manager import TodoManager
 _LOGGER = logging.getLogger(__name__)
 
 PLATFORMS = ["sensor"]
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
