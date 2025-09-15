59df28acde7777e158efe1a8e4d59323a46adea7
Blaine Venturine <blaineventurine@gmail.com>
2025-09-14 20:05:07 -0500

    update version

--- custom_components/simple_inventory/setup.py
+++ custom_components/simple_inventory/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="simple_inventory",
-    version="0.3.4",
+    version="0.3.5",
     packages=find_packages(),
     include_package_data=True,
     package_data={
